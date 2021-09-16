ff7ba40ec30f7b32a7da9ea1bfde2147147b3d72
Natalia Balmasheva <89969461+balmashevani@users.noreply.github.com>
2021-09-16 12:45:56 +0300

    Delete 09.09 names.py

--- 09.09 names.py
+++
@@ -1,9 +0,0 @@
-names = ['Балмашева Наталья Игоревна', 'Виноградова Юлия Игоревна', 'Путин Владимир Владимирович']
-def initials(name):
-    l = name.split()
-    return l[0] + ' '+ l[1][0:1] + '.' + l[2][0:1] + '.'
-
-def initials_more(names):
-    return [initials(name) for name in names]
-
-print(initials_more(names))
