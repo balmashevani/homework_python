79c2462eec0abd9a1a30e11e802cf7bfba0c852f
Natalia Balmasheva <89969461+balmashevani@users.noreply.github.com>
2021-09-16 12:52:17 +0300

    Add files via upload

---
+++ 11.09.py
@@ -0,0 +1,44 @@
+import string
+import random
+
+s = string.ascii_letters
+def let(s):
+    print(random.choice(s))
+    
+let(s)
+
+def create(num):
+    result = ''
+    for i in range (num):
+        result+=random.choice(string.ascii_letters)
+    return result
+
+
+def count_str(s):
+    big = 0
+    small = 0
+    for sym in s:
+        if sym.isupper():
+            big += 1
+        else:
+            small += 1
+    if big > small:
+        return 1
+    elif small > big:
+        return 0
+    else: 
+        return -1
+a = create(8)
+print(a)                
+print(count_str(a))  
+
+s = string.ascii_letters
+def let(s):
+    print(random.choice(s))
+    
+let(s)
+
+def f(length, num):
+    return [create(length) for i in range(num)]
+    
+print(f(8, 10))
